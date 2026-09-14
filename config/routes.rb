82a9a30061a5b9392e8e499766579061ca84a020
javinaxxa <cbastiasjaviera@gmail.com>
2026-09-14 20:54:22 -0300

    Lab 6 act

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   root "pages#home"
 
-  get "services", to: "pages#services", as: :services
+  get "services", to: "service_types#index", as: :services
   get "visit", to: "pages#visit", as: :visit
   get "about", to: "pages#about", as: :about
+
+  resources :customers,     only: [:index, :show]
+  resources :bikes,         only: [:index, :show]
+  resources :repairs,       only: [:index, :show]
+  resources :service_types, only: [:index, :show]
+  resources :staff_members, only: [:index, :show]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
