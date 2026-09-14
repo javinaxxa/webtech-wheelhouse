518a6dc3251117b441b2a3ccd1e1cd309413bd82
javinaxxa <cbastiasjaviera@gmail.com>
2026-09-14 20:49:26 -0300

    Lab 6: associations

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -1,2 +1,6 @@
 class Repair < ApplicationRecord
+  belongs_to :bike
+  belongs_to :received_by, class_name: "StaffMember"
+  belongs_to :assigned_mechanic, class_name: "StaffMember", optional: true
+  has_many :repair_line_items
 end
--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,2 +1,4 @@
 class StaffMember < ApplicationRecord
+  has_many :received_repairs, class_name: "Repair", foreign_key: :received_by_id
+  has_many :assigned_repairs, class_name: "Repair", foreign_key: :assigned_mechanic_id
 end
